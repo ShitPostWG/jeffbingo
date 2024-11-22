59e1843cf87de99fd07f7f5239b387d18a12e4d6
alexgmin <alexgmin@gmail.com>
2024-11-22 22:56:53 +0100

    Autosave, autoload and cryptomining

--- tailwind.js
+++ tailwind.js
@@ -12,6 +12,29 @@ function randomLoad(){
       li.innerText = s;
       bingo.appendChild(li);
       textFit(li, {alignHoriz: true, alignVert: true, multiLine: true, alignVertWithFlexbox: true});
-      li.addEventListener("click", function(){this.classList.toggle("active")})
+      li.addEventListener(
+        "click",
+        toggleBingoCell
+      )
     });
+    storeBingo()
+}
+
+function toggleBingoCell(){
+  this.classList.toggle("active")
+  storeBingo()
+}
+
+function storeBingo(){
+  localStorage.setItem("bingo", document.querySelector("#bingo").innerHTML)
+}
+
+function loadBingo(){
+  let bingo = localStorage.getItem("bingo")
+  // If we don't run textFit first the css they add is not loaded, and we don't have css for .textFitAlignVertFlex
+  randomLoad() // The user won't notice we load a random bingo first
+  if (bingo) {
+    document.querySelector("#bingo").innerHTML = bingo
+    document.querySelectorAll('#bingo > li').forEach((e) => e.addEventListener("click", toggleBingoCell))
+  }
 }
